de576d5b8dcc189e2424f38ec9d06fa5cc9cc245
Sujit Kumar <91783399+sujitkumarmasai@users.noreply.github.com>
2022-01-23 16:38:50 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,49 +1,45 @@
 
 import './App.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
 
 function App() {
   return (
-      <div className="App">
+    <div className="App">
       <div>
-        <img src="./images/adidas.png.png" alt='' />
+        <img className='icon' src="./images/adidas.png.png" alt='' />
       </div>
-    <div className='Top'>
-       <h1>PAYMENT METHOD</h1>
-       <p>All transactions are safe and secure</p>
-       </div>
-       <hr style={{width:"600px",marginLeft:"150px",marginTop:"20px"}}></hr>
-       <div className='payment'>   
-       <h4>Credit/Debit Card</h4>      
-       <p>You may be directed to your bank's 3D secure process to authenticate your information.</p>
-       </div>
-       <form>
-         <input type="number" placeholder='Card Number *'/><br/>
-         <input type="text" placeholder='Name On Card *'/><br/>
-         <input className='second' type="text" placeholder='MM/YY *'/>
-         <input className='third' type="number" placeholder='CVV *'/>
-         <hr style={{width:"600px",marginLeft:"150px",marginTop:"20px"}}></hr>
-         <h4>Cash on Delivery</h4>
-       </form>
-       
-       <button> <a className='img' href='https://www.delaneystudios.com.au/uploads/1/1/8/3/118392473/payment-successful_orig.png'>PLACE ORDER</a></button>
-       <div className='rightsideup'>
+      <h1 style={{marginLeft:70}}>YOUR BAG</h1>
+      <h5 style={{marginLeft:70}}>TOTAL (1 item) ₹1 979.00</h5>
+      <div className='top'>
+        <h3>40% - 60% OFF</h3>
+        <p>EXTRA 15% OFF ON ORDERS ABOVE ₹3499</p>
+        <p>SHOP NOW</p>
+      </div><br/><br/>
+      <div>
+        <img className='pant'  src="./images/menspant.jpg" alt='' />
+      </div>
+      <div className='details'>
+      <p>Q3 BLUV SERE PT</p>
+      <p>BLACK</p>
+      <p>SIZE: M</p>
+      <h4>LOW IN STOCK</h4>
+      <h5>Quantity 1</h5>
+      </div>
+      <button className='checkleft'>CHECKOUT</button>
+      <div className='rightsideup'>
        <h3>ORDER SUMMARY</h3>
        <div>DELIVERY<span style={{marginLeft:"150px"}}>FREE</span></div>
        <h5>TOTAL</h5>
        <p2>(inclusive of all taxes)<span style={{marginLeft:"40px"}}> ₹ 1979.00</span></p2>
        </div>
-
-       <div className='rightsidedown'>
-       <h3>ORDER DETAILS</h3>
-       <h5>DELIVERY OPTION</h5>
-       <p3>Standard Delivery</p3><br/>
-       <p4> within 3-9 Business Days FREE</p4>
+       <div className='down'>
+         <h5>NEED HELP?</h5>
+         <p><a href=''>Delivery</a></p>
+         <p><a href=''> Return & Refund</a></p>
+         <p><a href=''> Ordering & Payment</a></p>
+         <p><a href=''>Promotions & Vouchers</a></p>
        </div>
-
     </div>
-   
   );
 }
+
 export default App;
